ef6c7aafa758cf0ac6cbacda7de419179385432b
relicavaiden <dirrickricks@gmail.com>
2022-11-07 09:16:11 -0600

    VC-6 Test Added

--- test/test_variant_calling.py
+++ test/test_variant_calling.py
@@ -36,6 +36,30 @@ def test_variant_calling_null():
     variant_set = variant_calling_for_one_sam_record(sam_record)
     assert not variant_set
 
+def test_variant_calling_null_2():
+    """
+    Test the null case where there are no variants in the alignment
+    """
+    sam_record = {
+        "QNAME": "SRR1518133.27",
+        "FLAG": "16",
+        "RNAME": "chr15",
+        "POS": "102500878",
+        "MAPQ": "0",
+        "CIGAR": "76M",
+        "RNEXT": "*",
+        "PNEXT": "0",
+        "TLEN": "0",
+        "SEQ": "TGCTGGACTTTGGACTGATGATGCTCTTTTTATATATTTAACTTTTTAAAAAAGCCTCTTTTCTTTCTTTTTACCA",
+        "QUAL": "DFEFFFHHHHGIHGIJJJJJIJJJJJJJJJJJIJJJIGJJIIIJJJIJIJJJIIIGHGIIIHHDGHHHFFFFDCB@",
+        "NM": "NM:i:0",
+        "MD": "MD:Z:76",
+        "AS": "AS:i:76",
+        "XS": "XS:i:76",
+        "XA": "XA:Z:chr1,+30212,76M,0;chr19,+71819,76M,0;chr2,-114340752,76M,0;chr9,+29990,76M,0;chr12,-73401,10M2I64M,2;",
+    }
+    variant_set = variant_calling_for_one_sam_record(sam_record)
+    assert not variant_set
 
 def test_variant_calling_for_one_sam_record():
     """
